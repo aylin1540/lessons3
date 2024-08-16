02f8eac31dd93bfae3f424b5c4a231fb4432d673
aylin1540 <kanaylin759@gmai.com>
2024-08-16 06:16:47 -0700

    HW

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,36 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        double[] array = {2.5, 3.7, -1.2, 4.6, -3.4, 2.9, 5.1, -2.3, 4.4, 6.6, -7.8, 1.9, 3.3, -0.5, 8.0};
+
+
+        int firstNegativeIndex = -1;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < 0) {
+                firstNegativeIndex = i;
+                break;
+            }
+        }
+
+
+        if (firstNegativeIndex != -1) {
+            double sum = 0;
+            int count = 0;
+
+
+            for (int i = firstNegativeIndex + 1; i < array.length; i++) {
+                if (array[i] > 0) {
+                    sum += array[i];
+                    count++;
+                }
+            }
+
+
+            double average = count > 0 ? sum / count : 0;
+            System.out.println("Среднее арифметическое положительных чисел после первого отрицательного: " + average);
+        } else {
+            System.out.println("Отрицательных чисел в массиве не");
         }
     }
 }
+
